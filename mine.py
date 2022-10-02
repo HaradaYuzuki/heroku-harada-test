4c55b8b379083387b6fef7f97457cb136ccb8fcd
原田佑月 <yuzukiharada1006gmail.com>
2022-10-02 10:23:36 +0900

    My first commit

---
+++ mine.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, abort, jsonify
+import os
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "hello world!"
