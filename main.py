d1d913514211a4efe03749d63b7da7b74722c37d
原田佑月 <yuzukiharada1006gmail.com>
2022-10-30 10:36:38 +0900

    my change pythonv commit

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from flask import Flask, request, abort, jsonify
-import os
-from time import sleep
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-
+from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "hello world!"
+@app.route('/')
+def hello():
+    name = "Hello World"
+    return name
+
+@app.route('/good')
+def good():
+    name = "Good"
+    return name
+
+## おまじない
+if __name__ == "__main__":
+    app.run(debug=True)
