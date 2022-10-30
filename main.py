cc28e381145f51545b0070e83e4466261b74fb00
原田佑月 <yuzukiharada1006gmail.com>
2022-10-30 10:48:03 +0900

    my second comit

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    name = "Hello World"
-    return name
+def index():
+    return 'hello, world'
 
-@app.route('/good')
-def good():
-    name = "Good"
-    return name
-
-## おまじない
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
